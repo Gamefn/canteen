c33043b4d710edfd10c98702498bd2ba03a87cf1
Gamefn <levigotsweden@gmail.com>
2025-06-13 03:39:53 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,43 +1,12 @@
-module.exports.config = {
-  api: {
-    bodyParser: false,
-  },
-};
+import webview
 
-module.exports.default = async function handler(req, res) {
-  const targetUrl = "https://kitsu.eu.pythonanywhere.com" + req.url;
+webview.create_window(
+    'CGIS Canteen PoS',
+    'https://kitsu.eu.pythonanywhere.com',
+    width=1200,
+    height=800,
+    resizable=True,
+    js_api=None
+)
 
-  console.log(`[Proxy] ${req.method} ${req.url}`);
-
-  try {
-    const chunks = [];
-    for await (const chunk of req) {
-      chunks.push(chunk);
-    }
-
-    const body = chunks.length > 0 ? Buffer.concat(chunks) : undefined;
-
-    const response = await fetch(targetUrl, {
-      method: req.method,
-      headers: {
-        ...req.headers,
-        host: "kitsu.eu.pythonanywhere.com",
-        "content-type":
-          req.headers["content-type"] || "application/x-www-form-urlencoded",
-      },
-      body,
-    });
-
-    const buffer = await response.arrayBuffer();
-
-    const contentType = response.headers.get("content-type");
-    if (contentType) {
-      res.setHeader("Content-Type", contentType);
-    }
-
-    res.status(response.status).send(Buffer.from(buffer));
-  } catch (err) {
-    console.error("[Proxy Error]", err);
-    res.status(500).send("Proxy error");
-  }
-};
+webview.start(debug=False)
